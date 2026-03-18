fe7bf6a8f5b3a0f4f27431bb1e2d598a4fcccafe
gomoritz <theincxption@gmail.com>
2026-03-18 12:19:06 +0100

    Enable iCloud sync for SwiftData store

--- gym-bro/GymBroApp.swift
+++ gym-bro/GymBroApp.swift
@@ -17,7 +17,8 @@ struct GymBroApp: App {
         ])
         let modelConfiguration = ModelConfiguration(
             schema: schema,
-            isStoredInMemoryOnly: false
+            isStoredInMemoryOnly: false,
+            cloudKitDatabase: .automatic
         )
 
         do {
